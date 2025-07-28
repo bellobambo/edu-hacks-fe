d2b3e14219d119e04773a7fa5f46d6efc710525b
bellobambo <bellobambo21@gmail.com>
2025-07-28 21:47:41 +0100

    adjust file upload

--- src/components/ExamList.tsx
+++ src/components/ExamList.tsx
@@ -191,7 +191,7 @@ export default function ExamList() {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_DEEPSEEKAPI}`,
+            Authorization: `Bearer ${import.meta.env.VITE_API_KEY_DEEPSEEKAPI}`,
           },
           body: JSON.stringify({
             text: lectureNoteText,
